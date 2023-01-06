3c697264bb5ef4f5e532d0386a09cb01f7ec54a8
Johab Freitas <freitasjohab@gmail.com>
2023-01-06 16:06:28 -0300

    First commit

---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.services;
+
+/**
+ *
+ * @author johab
+ */
+public class PaypalService implements OnlinePaymentService {
+
+    @Override
+    public double paymentFee(double amount) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double interest(double amount, int months) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
